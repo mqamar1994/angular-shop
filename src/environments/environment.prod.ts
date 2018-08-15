88848724d494bafb0b2b63a07f57875c7a108bc2
Muhammad Qamaruddin <mqamar1994@gmail.com>
2018-08-15 03:00:19 -0400

    first commit for angular shop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBxYmjYZX0waU8YhdtRI6_M2XJ7Y0qCgiw",
+    authDomain: "angularshop-123.firebaseapp.com",
+    databaseURL: "https://angularshop-123.firebaseio.com",
+    projectId: "angularshop-123",
+    storageBucket: "angularshop-123.appspot.com",
+    messagingSenderId: "1076440502972"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,16 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBxYmjYZX0waU8YhdtRI6_M2XJ7Y0qCgiw",
+    authDomain: "angularshop-123.firebaseapp.com",
+    databaseURL: "https://angularshop-123.firebaseio.com",
+    projectId: "angularshop-123",
+    storageBucket: "angularshop-123.appspot.com",
+    messagingSenderId: "1076440502972"
+  }
 };
-
-/*
- * In development mode, for easier debugging, you can ignore zone related error
- * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
- * because it will have a performance impact when errors are thrown
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
